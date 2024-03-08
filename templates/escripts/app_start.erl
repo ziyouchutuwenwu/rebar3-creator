69aa8b99c42fd2ef0c94406bce44b729ae86a5a0
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2024-03-08 15:49:21 +0800

    .

--- templates/escripts/app_start.erl
+++ templates/escripts/app_start.erl
@@ -1,13 +1,12 @@
--module(on_app_start).
+-module(app_start).
 
 -export([main/1]).
--export([interprete_modules/0]).
+-export([interprete/0]).
 
 main(_Args) ->
-  io:format("~n"),
-  interprete_modules().
+  io:format("~n").
 
-interprete_modules() ->
+interprete() ->
   % 如果有多个依赖项目，也只是在主模块里面调用一次
   int:i([M || {M, _} <- code:all_loaded(), not code:is_sticky(M), int:interpretable(M) =:= true]),
   io:format("输入 int:interpreted(). 或者 il(). 查看模块列表~n").
